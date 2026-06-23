50a8112c4677013fb9d173b595b95b82c2dd40de
ketan ohal <mailmequicker@gmail.com>
2026-06-24 01:42:41 +0530

    Variable and datatype

---
+++ 01-python/day01_variables.py
@@ -0,0 +1,25 @@
+#variable is a named container used to store data in memory.
+name = "ketan"
+
+#basic data types in python
+#1. int - integer data type
+age = 25
+
+#2. float - floating point data type
+height = 5.9
+
+#3. str - string data type
+city = "New York"
+
+#4. bool - boolean data type
+is_student = True
+
+
+print(name)
+
+#checking type of variable
+
+print(type(name))
+
+#user input
+name = input("Enter the username: ")
